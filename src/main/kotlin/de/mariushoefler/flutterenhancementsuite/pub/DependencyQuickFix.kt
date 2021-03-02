9731ed85d73e3f73990f402c2426c8289875cb70
Marius Höfler <marius.hoefler@yahoo.de>
2021-03-02 01:03:34 +0100

    Fix document locked by PSI write operations

--- src/main/kotlin/de/mariushoefler/flutterenhancementsuite/pub/DependencyQuickFix.kt
+++ src/main/kotlin/de/mariushoefler/flutterenhancementsuite/pub/DependencyQuickFix.kt
@@ -1,13 +1,12 @@
 package de.mariushoefler.flutterenhancementsuite.pub
 
 import com.intellij.codeInspection.LocalQuickFixOnPsiElement
-import com.intellij.lang.Language
+import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.tree.IElementType
 import de.mariushoefler.flutterenhancementsuite.utils.FlutterProjectUtils
+import org.jetbrains.yaml.YAMLElementGenerator
 
 class DependencyQuickFix(
     psiElement: PsiElement,
@@ -19,13 +18,11 @@ class DependencyQuickFix(
     override fun getText(): String = "Update package" + if (!forcePubGet) " without running pub get" else ""
 
     override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
-        val factory = JavaPsiFacade.getInstance(project).elementFactory
-        val psiExpression = factory.createDummyHolder(
-            "^$latestVersion",
-            IElementType("text", Language.findLanguageByID("yaml")),
-            null
-        )
-        startElement.replace(psiExpression)
+        val expression = YAMLElementGenerator(project).createDummyYamlWithText("^$latestVersion").firstChild
+
+        runUndoTransparentWriteAction {
+            startElement.replace(expression)
+        }
 
         if (forcePubGet) FlutterProjectUtils.runPackagesGet(file.virtualFile)
     }
