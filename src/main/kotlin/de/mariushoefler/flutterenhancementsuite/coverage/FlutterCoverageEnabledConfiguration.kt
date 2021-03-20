293ad4c925c3564162fd35c10c6dbfc9c4131695
Marius Höfler <marius.hoefler@yahoo.de>
2021-03-20 01:52:36 +0100

    Fix path bugs

--- src/main/kotlin/de/mariushoefler/flutterenhancementsuite/coverage/FlutterCoverageEnabledConfiguration.kt
+++ src/main/kotlin/de/mariushoefler/flutterenhancementsuite/coverage/FlutterCoverageEnabledConfiguration.kt
@@ -5,8 +5,6 @@ import com.intellij.coverage.CoverageRunner
 import com.intellij.execution.configurations.RunConfigurationBase
 import com.intellij.execution.configurations.coverage.CoverageEnabledConfiguration
 import io.flutter.pub.PubRootCache
-import java.io.File
-import java.net.URI
 
 /**
  * Coverage Enabled Configuration for Flutter
@@ -22,7 +20,7 @@ class FlutterCoverageEnabledConfiguration(conf: RunConfigurationBase<*>) : Cover
     init {
         coverageRunner = CoverageRunner.getInstance(FlutterCoverageRunner::class.java)
         PubRootCache.getInstance(project).getRoot(project.projectFile)?.let {
-            myCoverageFilePath = File(URI("${it.root}/coverage/lcov.info")).path
+            myCoverageFilePath = "${it.root.canonicalPath}/coverage/lcov.info"
         }
         currentCoverageSuite = CoverageDataManager.getInstance(project)
             .addCoverageSuite(this)
