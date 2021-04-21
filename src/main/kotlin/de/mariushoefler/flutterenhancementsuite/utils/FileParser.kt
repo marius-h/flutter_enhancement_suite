067f4d3043c0ee73338d3993b9ac2a2ff0ed4e78
Marius Höfler <marius.hoefler@yahoo.de>
2021-04-21 16:36:07 +0200

    Fix compatibility problems

--- src/main/kotlin/de/mariushoefler/flutterenhancementsuite/utils/FileParser.kt
+++ src/main/kotlin/de/mariushoefler/flutterenhancementsuite/utils/FileParser.kt
@@ -1,7 +1,5 @@
 package de.mariushoefler.flutterenhancementsuite.utils
 
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.mapWithProgress
 import com.intellij.psi.PsiFile
 import de.mariushoefler.flutterenhancementsuite.exceptions.GetLatestPackageVersionException
 import de.mariushoefler.flutterenhancementsuite.models.VersionDescription
@@ -17,20 +15,15 @@ class FileParser(private val file: PsiFile) {
     }
 
     private fun getVersionsFromFile(): MutableList<VersionDescription> {
-        val progressIndicator = ProgressManager.getInstance().progressIndicator
         val problemDescriptionList = mutableListOf<VersionDescription>()
         val packageLines = file.readPackageLines()
-        var i = 0.0
-        packageLines.mapWithProgress(progressIndicator) { pair, progress ->
-            i++
-            progress.text = "Fetching latest version of " + pair.first
-            progress.fraction = (i / packageLines.size)
-            mapToVersionDescription(pair)
-        }.forEach { versionDescription ->
-            if (versionDescription.latestVersion != versionDescription.currentVersion) {
-                problemDescriptionList.add(versionDescription)
+        packageLines
+            .map { pair -> mapToVersionDescription(pair) }
+            .forEach { versionDescription ->
+                if (versionDescription.latestVersion != versionDescription.currentVersion) {
+                    problemDescriptionList.add(versionDescription)
+                }
             }
-        }
         return problemDescriptionList
     }
 
